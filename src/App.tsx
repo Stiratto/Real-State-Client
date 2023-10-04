dc91c09f121ba900dba209d705dc6e73dd7f893e
Cesar-Moran <cesarmorangithub@gmail.com>
2023-10-04 11:34:15 -0500

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
-export const App = () => {
-  return <div>App</div>;
-};
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/about";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/signin" element={<SignIn />}></Route>
+      <Route path="/signout" element={<SignOut />}></Route>
+      <Route path="/profile" element={<Profile />}></Route>
+    </Routes>
+  );
+}
