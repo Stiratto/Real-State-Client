1566f72172e486414c3e718887c090aa39230021
Cesar-Moran <cesarmorangithub@gmail.com>
2023-10-06 11:08:57 -0500

    Complete profile page UI

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,3 +1,47 @@
-export default function Profile() {
-  return <div>Profile</div>;
+import { useSelector } from "react-redux";
+
+function Profile() {
+  const selectAuth = (state: any) => state.user;
+  const { currentUser } = useSelector(selectAuth);
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7 ">Profile</h1>
+      <form className="flex flex-col gap-5">
+        <img
+          src={currentUser.avatar}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center my-2"
+        ></img>
+
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="border p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Update
+        </button>
+      </form>
+
+      <div className="my-5 flex justify-between">
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+    </div>
+  );
 }
+
+export default Profile;
